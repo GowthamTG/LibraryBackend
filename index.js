7ec7e873c0f8655906923a1d2ac2a66bdbb171ff
Gowtham T G <gowthamgopinath1@gmail.com>
2022-08-25 12:06:44 +0530

    CORRRRRRRRRRRR

--- index.js
+++ index.js
@@ -8,23 +8,13 @@ const errorLogger = require("./utils/errorLogger");
 const bookRoutes = require("./routes/book.routes");
 
 const app = express();
-app.use(express.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-// app.use(function (req, res, next) {
-//   //Enabling CORS
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
-//   );
-//   next();
-// });
+
 app.use(cors());
 
-app.use(bookRoutes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+app.use(bookRoutes);
 app.use(errorLogger);
 
 const PORT = process.env.PORT || 5000;
