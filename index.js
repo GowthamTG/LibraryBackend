317e376359f510f04d1b720010bb192e9cfad280
Gowtham T G <gowthamgopinath1@gmail.com>
2022-08-25 11:41:18 +0530

    CORS ff

--- index.js
+++ index.js
@@ -2,20 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
+require("dotenv").config();
 
 const errorLogger = require("./utils/errorLogger");
 const bookRoutes = require("./routes/book.routes");
 
 const app = express();
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
-  );
-  next();
-});
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
