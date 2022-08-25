77e5a3995b131d838c7a956cb15704fc10489cd0
Gowtham T G <gowthamgopinath1@gmail.com>
2022-08-25 12:18:39 +0530

    TT

--- index.js
+++ index.js
@@ -14,8 +14,12 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(bookRoutes, cors());
-app.use(errorLogger, cors());
+app.get("/test", (req, res, next) => {
+  res.send({ message: "Working" });
+});
+
+app.use(bookRoutes);
+app.use(errorLogger);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
