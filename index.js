e7176622c7e3114ba04655d7ee8d42260b2f30d4
Gowtham T G <gowthamgopinath1@gmail.com>
2022-08-25 11:36:47 +0530

    CORS

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ const errorLogger = require("./utils/errorLogger");
 const bookRoutes = require("./routes/book.routes");
 
 const app = express();
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+});
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
