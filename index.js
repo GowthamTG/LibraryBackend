e0726da968fc0b6f701dfa7c49dc1404fc236a2b
Gowtham T G <gowthamgopinath1@gmail.com>
2022-08-25 12:28:54 +0530

    CORS

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
-require("dotenv").config();
 
 const errorLogger = require("./utils/errorLogger");
 const bookRoutes = require("./routes/book.routes");
@@ -22,4 +21,10 @@ app.use(bookRoutes);
 app.use(errorLogger);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(process.env.PORT || 5000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
