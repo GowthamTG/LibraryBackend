48253d0f1629a5d12b4b13676e4bbc9bdbb92eb0
Gowtham T G <gowthamgopinath1@gmail.com>
2022-08-25 11:45:21 +0530

    CORSSS

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const app = express();
 app.use(express.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(bodyParser.json());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 app.use(cors());
 
